78e04f4e165909dcab29fb875ebd7253c4649332
sqandan <matchawat@qq.com>
2021-01-29 19:27:57 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,26 @@
+# !/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+@Author         :  matchawat
+@Email          :  aaadmin@88.com
+------------------------------------
+@File           :  start.py
+@Version        :  
+@Description    :  
+@CreateTime     :  2021/1/26/0026 20:08
+------------------------------------
+@Software       :  PyCharm
+"""
+
+import sys
+from main.login_pane import *
+from PyQt5.QtWidgets import QApplication
+
+################################################
+#######程序入门
+################################################
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    login_window = login_pane(mode=0)
+    login_window.show()
+    sys.exit(app.exec_())
